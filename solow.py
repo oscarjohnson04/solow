a53a2b93d97cfa51e17842851dbf7d44b003368a
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-12-26 19:37:46 -0500

    solow.py

--- solow.py
+++ solow.py
@@ -135,10 +135,10 @@ def romer_A_path(A0, lambda_RD, phi, theta, N_path):
     A = np.zeros(T)    
     A[0] = A0    
     N_norm = N_path / N_path[0]        
-    for t in range(1, T):        
-        L_A = theta * N_norm[t-1]                        
-        gA = lambda_RD * (L_A ** phi)         
-        A[t] = A[t-1] * (1 + gA) 
+    for t in range(1, T):
+        L_A = theta * N_norm[t-1]
+        gA = lambda_RD * (L_A ** phi)
+        A[t] = A[t-1] * (1 + gA)
     
     return A
 
