1b5db5af4487e6f4e6d72234ca70df715e9df9ce
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-12-26 20:17:06 -0500

    solow.py

--- solow.py
+++ solow.py
@@ -100,8 +100,8 @@ with st.sidebar:
         "Capital share (α)",
         min_value=0.01, max_value=0.99, value=0.33, step=0.01, format="%.2f"
     )
-    lambda_RD = st.number_input("R&D effectiveness (λ)", min_value=0.0, max_value=1.0, value=0.2, step=0.10, format="%.2f")
-    phi = st.number_input("R&D returns to scale (φ)", min_value=0.0, max_value=1.0, value=0.25, step=0.01)
+    lambda_RD = st.number_input("R&D effectiveness (λ)", min_value=0.0, max_value=1.0, value=0.05, step=0.10, format="%.2f")
+    phi = st.number_input("R&D returns to scale (φ)", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
     theta = st.number_input("R&D labor share (θ)", min_value=0.0, max_value=1.0, value=0.02, step=0.01)
     delta = st.number_input("Capital Depreciation (δ)", min_value=0.0, max_value=1.0, value=0.05, step=0.01, format="%.2f")
     T = st.number_input("Simulation periods (T)", min_value=1, max_value=2000, value=100, step=1)
