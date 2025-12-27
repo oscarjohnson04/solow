7dbe06e632628d86dd62b7d4cf5115057275114b
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-12-26 20:30:49 -0500

    solow.py

--- solow.py
+++ solow.py
@@ -271,5 +271,5 @@ st.download_button(
 
 with st.expander("ℹ️ Information about the model"):
     st.write("The model is a Solow growth model that includes endogenous TFP growth based on Paul Romer's research. \n
-            "Calculations for the model can be found in the GitHub repo. \n
-            "For population growth, more weight is put on more recent data.")
+            Calculations for the model can be found in the GitHub repo. \n
+            For population growth, more weight is put on more recent data.")
